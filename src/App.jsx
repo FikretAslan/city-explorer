b6fe8a5ff6bf1d35dfbb7576f96d0ddd1cfa8983
Fikret Aslan <f.aslan0191@gmail.com>
2023-10-30 14:14:35 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  return <></>;
-}
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
 
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    const res = await axios.get(API);
+
+    setLocation(res.data[0]);
+  }
+
+  return (
+    <>
+      <h1>Location APIs</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Location" />
+        <button>Explore!</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+    </>
+  );
+}
 export default App;

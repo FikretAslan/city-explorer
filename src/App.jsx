37b075983e7b08239186abdff13819ddbeef5cd7
Fikret Aslan <f.aslan0191@gmail.com>
2023-10-30 16:01:33 +0000

    latandlon

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 function App() {
   const [location, setLocation] = useState({});
   const [search, setSearch] = useState("");
+  const [map, setMap] = useState({});
 
   function handleChange(event) {
     setSearch(event.target.value);
+  }
+
+  function displayMap(event) {
+    setMap(event.target.value);
   }
 
   async function getLocation(event) {
     event.preventDefault();
 
     const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
-    console.log(API);
 
     const res = await axios.get(API);
 
@@ -30,8 +34,9 @@ function App() {
         <input onChange={handleChange} placeholder="Location" />
         <button>Explore!</button>
       </form>
-
-      <h2>{location.display_name}</h2>
+      <h2>Location: {location.display_name}</h2>
+      <h2>Longitude: {location.lon}</h2>
+      <h2>Latitude: {location.lat}</h2>
     </>
   );
 }

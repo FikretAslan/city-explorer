09ff84f9711d9fd698a027bf911b795ca2cda3eb
Fikret Aslan <f.aslan0191@gmail.com>
2023-11-03 13:33:35 +0000

    changes to weather

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import axios from "axios";
 import { useState } from "react";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY_WEATHER = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
   const [location, setLocation] = useState({});
   const [search, setSearch] = useState("");
   const [number, setNumber] = useState(10);
+  const [weather, setWeather] = useState([]);
 
   function handleChange(event) {
     setSearch(event.target.value);
@@ -21,15 +23,29 @@ function App() {
     const res = await axios.get(API);
 
     setLocation(res.data[0]);
+
+    getWeather(response.data[0]);
   }
 
   function handleNumber(mod) {
     setNumber(number + mod);
   }
 
+  async function getWeather(tempLocation) {
+    const API = `https://api.weatherbit.io/v2.0/current?lat=${tempLocation.lat}&lon=${tempLocation.lon}&key=${API_KEY_WEATHER}&include=minutely`;
+    const response = await axios.get(API);
+    const processedData = response.data.data.map((day) => {
+      return {
+        description: day.weather.description,
+        date: day.datetime,
+      };
+    });
+    setWeather(processedData);
+  }
+
   return (
     <>
-      <h1>APIs</h1>
+      <h1>City Explorer Hub</h1>
       <form onSubmit={getLocation}>
         <input onChange={handleChange} placeholder="Location" />
         <button>Get Location</button>
@@ -48,6 +64,12 @@ function App() {
       )}
 
       <h2>{location.display_name}</h2>
+      <h2>{location.lat}</h2>
+      <h2>{location.lon}</h2>
+
+      {weather.map((day) => {
+        return <p>The weather is currently {day.description}</p>;
+      })}
     </>
   );
 }
